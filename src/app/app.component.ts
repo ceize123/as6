344324106a8d86775c4165879e452d2ff5c14cef
ytkuo <hahaeating1@gmail.com>
2022-07-06 21:59:26 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,17 @@
+/********************************************************************************* 
+* 
+* WEB422 – Assignment 06 
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this 
+* assignment has been copied manually or electronically from any other source (including web sites) or 
+* distributed to other students. 
+* 
+* Name: ____Yi-Tso Kuo________ Student ID: __166577197________ Date: __Mar. 31, 2022______
+*
+* Angular App (Deployed) Link: __https://web-422-as6.vercel.app/__________
+*
+* User API (Heroku) Link: __https://fast-badlands-75912.herokuapp.com/________ 
+* 
+********************************************************************************/
 
 import { Component, OnInit } from '@angular/core';
 import { Router, Event, NavigationStart } from '@angular/router';
